f586108a14e45c3124db8c2ffcea4513d80d0158
TomJohnH <thasiow@onet.pl>
2023-02-13 17:19:26 +0100

    removed ttl

--- streamlit_app.py
+++ streamlit_app.py
@@ -235,7 +235,7 @@ def down_callback():
 # ---------------- data fetch ----------------
 
 
-@st.cache_data(ttl=600)
+@st.cache_data
 def fetch_data(level_name):
     df = pd.read_csv(level_name, sep=",", header=None)
     return df
