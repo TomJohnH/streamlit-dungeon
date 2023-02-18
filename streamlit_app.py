4ab3589edfcd6ef97a511c4ecf9a4326981d8ebf
TomJohnH <thasiow@onet.pl>
2023-02-18 10:12:09 +0100

    modularization in progress

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+import dungeon_config
 import json
 import numpy as np
 import pandas as pd
@@ -339,65 +340,9 @@ local_css("style.css")
 
 # ---------------- tilset dictionary ----------------
 
-tileset = {
-    "@": "https://oshi.at/ZMUu/avRY.gif",
-    "W": "https://thumbs2.imgbox.com/10/db/7zaxbIP8_t.png",  # wall
-    "FP": "https://thumbs2.imgbox.com/29/22/5rTLr6WH_t.png",  # floor_plain
-    # "FP": "https://oshi.at/PQkn/ExtR.png",  # floor 1 tilset 2
-    "CAT": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/other/cat.gif",  # cat
-    "M": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/other/monster.gif",  # monster, skeleton
-    "FS": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/floor_stain_1.png",
-    "E": "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==",
-    "FE3": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/floor_edge_3.png",  # floor_edge_3
-    "WON": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/Wall_outer_n.png",  # Wall_outer_n
-    "WOE": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/Wall_outer_e.png",  # Wall_outer_e
-    "WONE": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/Wall_outer_ne.png",  # Wall_outer_ne
-    "WOW": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/Wall_outer_w.png",  # Wall_outer_w
-    "WONW": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/Wall_outer_nw.png",  # wall_outer_nw
-    "WFR": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/Wall_front_right.png",  # wall front right
-    "WTR": "https://oshi.at/QpWg/Mfxv.png",  # wall top right
-    # "DK": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/wall_missing_brick_2.png",  # darkness
-    "WMB": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/wall_missing_brick_2.png",  # wall missing brick 1
-    "BOX": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/box.png",  # box
-    "DR": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/darkness_right.png",  # darkenss right
-    "DB": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/darkness_bottom.png",  # darkness bottom
-    "T": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/other/torch.gif",  # torch
-    "FMN1": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/floor_mud_n_1.png",  # floor_mud_n_1
-    "FMN2": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/floor_mud_n_2.png",  # floor_mud_n_2
-    "FMNE": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/floor_mud_ne.png",  # floor_mud_ne
-    "CGOF": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/chest_golden_open_full.png",  # chest_golden_open_full
-    "CGOO": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/chest_open_empty.png",  # chest_open_empty
-    "FL": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/Floor_ladder.png",  # Floor_ladder
-}
+tileset = dungeon_config.tileset
 
-tileset_movable = {
-    "@": True,
-    "W": False,
-    "FP": True,  # floor_plain
-    # "FP": "https://oshi.at/PQkn/ExtR.png",  # floor 1 tilset 2
-    "CAT": True,
-    "M": True,  # monster, skeleton
-    "FS": True,
-    "E": False,
-    "FE3": False,  # floor_edge_3
-    "WON": False,  # wall outer n
-    "WOE": False,  # wall outer e
-    "WONE": False,  # wall outer ne
-    "WOW": False,  # wall outer w
-    "WONW": False,  # wall_outer_nw
-    "WFR": False,  # wall front right
-    "WTR": False,  # wall top right
-    "DK": False,  # darkness
-    "WMB": False,  # wall missing brick
-    "BOX": False,  # box
-    "DR": False,  # darkenss right
-    "DB": False,  # darkness bottom
-    "T": False,  # torch
-    "FMN1": True,  # floor mud n1
-    "FMN2": True,
-    "FMNE": True,  # floor mud ne
-    "FL": True,
-}
+tileset_movable = dungeon_config.tileset_movable
 
 # ---------------- level renderer ----------------
 
